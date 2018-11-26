712c61551aa55bd18804a9d6b329fa41bf359ee7
Leandro <leandromfacchini@gmail.com>
2018-11-25 23:18:36 -0200

    Utiliznado fluent API

--- ProjetoModeloDDD/ProjetoModeloDDD.Domain/Entities/Cliente.cs
+++ ProjetoModeloDDD/ProjetoModeloDDD.Domain/Entities/Cliente.cs
@@ -14,6 +14,7 @@ namespace ProjetoModeloDDD.Domain.Entities
         public string Email { get; set; }
         public DateTime DataCadastro { get; set; }
         public bool Ativo { get; set; }
+        public virtual IEnumerable<Produto> Produtos { get; set; }
 
         public bool ClienteEspecial(Cliente cliente)
         {
